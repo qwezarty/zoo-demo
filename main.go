0f90bd9a127c3bd8ee73c1ec11e5b894891fc57d
qwezarty <qwezarty@gmail.com>
2019-06-18 17:27:10 +0800

    register to main

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/qwezarty/zoo-demo/apps"
+	"github.com/qwezarty/zoo-demo/apps/animal"
+	"github.com/qwezarty/zoo-demo/apps/zoo"
+	"github.com/qwezarty/zoo-demo/engine"
+)
+
+func main() {
+	// startup all managers
+	var router = gin.Default()
+	var db = engine.Startup()
+
+	// register all sub-routes
+	apps.Configure(db)
+	zoo.Configure(router, db)
+	animal.Configure(router, db)
+
+	log.Fatal(router.Run("0.0.0.0:30096"))
+}
