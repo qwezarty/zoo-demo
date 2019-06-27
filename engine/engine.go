0ce2b9ffdfc4a6a3b283ebaf65417875699cd385
qwezarty <qwezarty@gmail.com>
2019-06-27 14:03:52 +0800

    remove unnecessary code

--- engine/engine.go
+++ engine/engine.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mssql"
+	// _ "github.com/jinzhu/gorm/dialects/mssql"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/qwezarty/zoo-demo/models"
 )
